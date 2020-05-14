f86ff14fa521827f81173563c7707ff4e8cbda97
Nikolas Barwicki <nikolas.barwicki@gmail.com>
2020-05-14 14:32:08 +0200

    Add validation to the main form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,42 @@
 import React, { useContext } from 'react';
 import { Formik } from 'formik';
+import * as Yup from 'yup';
 import { GlobalContext } from 'context/GlobalState';
 import { ModalContext } from 'context/modalContext';
 import NumberInput from 'components/NumberInput';
 import cuid from 'cuid';
 import styled, { css } from 'styled-components';
+
+const StyledInputs = css`
+  -webkit-transition: all 0.2s ease-in-out;
+  -moz-transition: all 0.2s ease-in-out;
+  -ms-transition: all 0.2s ease-in-out;
+  -o-transition: all 0.2s ease-in-out;
+
+  &:focus {
+    border: 1px solid ${(props) => props.theme.blue};
+    box-shadow: 0 0 5px rgba(77, 124, 254, 0.5);
+    outline: none;
+  }
+`;
+
+const StyledNumberInput = styled(NumberInput)`
+  ${StyledInputs}
+
+  ${({ error }) =>
+    error &&
+    css`
+      border: 1px solid ${(props) => props.theme.red};
+      outline: none;
+
+      &:focus,
+      &:active {
+        box-shadow: ${(props) => props.theme.red} 0px 0px 5px 1px;
+        border: 1px solid ${(props) => props.theme.red};
+        outline: none;
+      }
+    `}
+`;
 
 const StyledForm = styled.form`
   display: grid;
@@ -17,6 +49,8 @@ const StyledForm = styled.form`
 `;
 
 const StyledSelect = styled.select`
+  ${StyledInputs}
+
   height: 4rem;
   padding: 1rem;
   border-radius: 1.5rem;
@@ -27,6 +61,8 @@ const StyledSelect = styled.select`
 `;
 
 const StyledInput = styled.input`
+  ${StyledInputs}
+
   height: 4rem;
   padding: 1rem;
   border-radius: 1.5rem;
@@ -34,6 +70,20 @@ const StyledInput = styled.input`
   font-family: inherit;
   font-weight: ${(props) => props.theme.bold};
   color: inherit;
+
+  ${({ error }) =>
+    error &&
+    css`
+      border: 1px solid ${(props) => props.theme.red};
+      outline: none;
+
+      &:focus,
+      &:active {
+        box-shadow: ${(props) => props.theme.red} 0px 0px 5px 1px;
+        border: 1px solid ${(props) => props.theme.red};
+        outline: none;
+      }
+    `}
 `;
 
 const StyledButton = styled.button`
@@ -73,6 +123,20 @@ const StyledHeading = styled.h2`
   margin-bottom: 2rem;
 `;
 
+const Error = styled.span`
+  color: ${(props) => props.theme.red};
+  font-size: ${(props) => props.theme.fontSize.s};
+`;
+
+const schema = Yup.object().shape({
+  name: Yup.string()
+    .min(1, 'The name is too short!')
+    .max(50, 'The name is too long!')
+    .required('Please enter the name'),
+  date: Yup.date().required('Please enter the date'),
+  amount: Yup.number().required('Please enter the amount'),
+});
+
 const Form = () => {
   const { addTransaction, categories, updateExpense, expenses } = useContext(GlobalContext);
   const { handleModal } = useContext(ModalContext);
@@ -83,7 +147,14 @@ const Form = () => {
     <div>
       <StyledHeading>Add new transaction</StyledHeading>
       <Formik
-        initialValues={{ id: cuid.slug(), category: 'Food', name: '', date: 0, amount: '' }}
+        initialValues={{
+          id: cuid.slug(),
+          category: 'Food',
+          name: '',
+          date: '',
+          amount: '',
+        }}
+        validationSchema={schema}
         onSubmit={(values) => {
           addTransaction(values);
           console.log(values);
@@ -121,7 +192,10 @@ const Form = () => {
                 type="text"
                 onChange={props.handleChange}
                 value={props.values.name}
+                autoComplete="off"
+                error={props.errors.name && props.touched.name}
               />
+              {props.errors.name && props.touched.name ? <Error>{props.errors.name}</Error> : null}
             </StyledLabel>
 
             <StyledLabel htmlFor="date">
@@ -133,16 +207,23 @@ const Form = () => {
                 onChange={props.handleChange}
                 value={props.values.date}
                 parse={props.parse}
+                error={props.errors.date && props.touched.date}
               />
+              {props.errors.date && props.touched.date ? <Error>{props.errors.date}</Error> : null}
             </StyledLabel>
 
             <StyledLabel htmlFor="amount">
               Amount
-              <NumberInput
+              <StyledNumberInput
                 value={props.values.amount}
                 onValueChange={(val) => props.setFieldValue('amount', val.floatValue)}
+                error={props.errors.amount && props.touched.amount}
               />
+              {props.errors.amount && props.touched.amount ? (
+                <Error>{props.errors.amount}</Error>
+              ) : null}
             </StyledLabel>
+
             <ButtonsWrapper>
               <StyledButton warning type="button" onClick={() => handleModal()}>
                 Cancel
